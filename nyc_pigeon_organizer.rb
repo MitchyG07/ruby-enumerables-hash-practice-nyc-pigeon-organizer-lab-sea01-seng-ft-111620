adcf8070e16705aec535b1c7e52ec4178ff5e5c4
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-10 17:28:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,9 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   final_array = data.each_with_object({}) do |(key, value), final_array| 
     value.each do |inner_key, names|
+      # binding.pry
       names.each do |name|
+        binding.pry 
         if !final_array[name]
           final_array[name] = {} 
         end 
